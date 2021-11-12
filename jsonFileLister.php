a5e8e636f996b97ef2adcfd7181879728ed07b02
crayleblanc <88908474+crayleblanc@users.noreply.github.com>
2021-11-12 13:28:38 -0600

    Update jsonFileLister.php

--- jsonFileLister.php
+++ jsonFileLister.php
@@ -1,6 +1,6 @@
 var jsonFiles = <?php $out = array();
 //this program will list out the names of the json files within the json_vaccine_files folder and store them as a javascript array
-foreach (glob('json_vaccine_files\*.json') as $filename) {
+foreach (glob('json_vaccine_files/*.json') as $filename) {
     $p = pathinfo($filename);
     $out[] = $p['filename'];
 }
