500a4882d91e3b20df5470cae7acf00ac3add59f
crayleblanc <88908474+crayleblanc@users.noreply.github.com>
2021-11-12 13:42:41 -0600

    Delete jsonFileLister.php

--- jsonFileLister.php
+++
@@ -1,7 +0,0 @@
-var jsonFiles = <?php $out = array();
-//this program will list out the names of the json files within the json_vaccine_files folder and store them as a javascript array
-foreach (glob('json_vaccine_files/*.json') as $filename) {
-    $p = pathinfo($filename);
-    $out[] = $p['filename'];
-}
-echo json_encode($out); ?>;
